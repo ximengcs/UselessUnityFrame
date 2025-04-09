79a1ae3caf7e75f8772cb2d8e6c9afc3b6de4ab0
ximengcs <ximengcs@163.com>
2025-04-09 16:26:51 +0800

    update

---
+++ Assets/Scripts/TestModule.cs
@@ -0,0 +1,32 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UselessFrame.Runtime;
+
+namespace TestGame
+{
+    public class TestModule : ModuleBase, IUpdater
+    {
+        protected override void OnInit(object param)
+        {
+            base.OnInit(param);
+            Debug.Log($"Test Module OnInit");
+        }
+
+        protected override async UniTask OnStart()
+        {
+            Debug.Log($"Test Module OnStart");
+            await UniTask.CompletedTask;
+        }
+
+        protected override async UniTask OnDestroy()
+        {
+            Debug.Log($"Test Module OnDestroy");
+            await UniTask.CompletedTask;
+        }
+
+        public void OnUpdate(float detalTime)
+        {
+
+        }
+    }
+}
