f27dcbd3d82d4b841962d4100595085574dd55f7
ximengcs <ximengcs@163.com>
2025-04-09 10:57:20 +0800

    cmd

--- Assets/Commands/TestCmd.cs
+++ Assets/Commands/TestCmd.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 namespace Game.Commands
 {
-    public class TestCmd : MonoBehaviour
+    public class TestCmd
     {
-        [Command("t1", MonoTargetType.Singleton)]
+        [RuntimeInitializeOnLoadMethod]
+        public static void Initialize()
+        {
+            QuantumRegistry.RegisterObject(new TestCmd());
+        }
+
+        [Command("t1", MonoTargetType.Registry)]
         public void PrintDebug()
         {
             Debug.Log("test");
         }
 
-        [Command("t2", MonoTargetType.Singleton)]
+        [Command("t2", MonoTargetType.Registry)]
         public void PrintWarn()
         {
             Debug.LogWarning("test");
         }
 
-        [Command("t3", MonoTargetType.Singleton)]
+        [Command("t3", MonoTargetType.Registry)]
         public void PrintError()
         {
             Debug.LogError("test");
