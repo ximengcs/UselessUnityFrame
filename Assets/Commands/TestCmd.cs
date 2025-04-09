2cc6ea53a80ce3468b0bbcd6a47ed6ffd2614914
ximengcs <ximengcs@163.com>
2025-04-09 11:04:13 +0800

    test

--- Assets/Commands/TestCmd.cs
+++ Assets/Commands/TestCmd.cs
@@ -13,18 +13,21 @@ namespace Game.Commands
         }
 
         [Command("t1", MonoTargetType.Registry)]
+        [CommandDescription("print test logs with debug")]
         public void PrintDebug()
         {
             Debug.Log("test");
         }
 
         [Command("t2", MonoTargetType.Registry)]
+        [CommandDescription("print test logs with warning")]
         public void PrintWarn()
         {
             Debug.LogWarning("test");
         }
 
         [Command("t3", MonoTargetType.Registry)]
+        [CommandDescription("print test logs with error")]
         public void PrintError()
         {
             Debug.LogError("test");
